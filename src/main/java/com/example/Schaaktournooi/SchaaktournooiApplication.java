112370b2581d089f645971dd8a77912e21c98c78
Kelvinoo <46557097+Kelvinoo@users.noreply.github.com>
2019-02-14 13:05:40 +0100

    Schaaktournooi

---
+++ src/main/java/com/example/Schaaktournooi/SchaaktournooiApplication.java
@@ -0,0 +1,14 @@
+package com.example.Schaaktournooi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaaktournooiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaaktournooiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/Schaaktournooi/SchaaktournooiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.Schaaktournooi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchaaktournooiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
