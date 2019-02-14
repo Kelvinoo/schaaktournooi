30238c9478b848324a4c2bcf223761e80dee0a92
Kelvinoo <46557097+Kelvinoo@users.noreply.github.com>
2019-02-14 17:04:21 +0100

    OPDRACHT 3 BIJNA AF

--- src/main/java/com/example/Schaaktournooi/api/SchaakEndpoint.java
+++ src/main/java/com/example/Schaaktournooi/api/SchaakEndpoint.java
@@ -6,6 +6,8 @@ import com.example.Schaaktournooi.model.Deelnemer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class SchaakEndpoint {
 
@@ -13,16 +15,29 @@ public class SchaakEndpoint {
     @Autowired
     private SchaakRepository mijnSchaakRepository;
 
-    @GetMapping("deelnemer")
+    @GetMapping("/deelnemer")
     public Iterable<Deelnemer> toonAlleDeelnemers(){
         Iterable<Deelnemer> deelnemers = mijnSchaakRepository.findAll();
         return deelnemers;
     }
 
-    @PostMapping("nieuweschaker")
+    @PostMapping("/deelnemer/add")
     public String addSchaker (@RequestBody Deelnemer deelnemer){
         mijnSchaakRepository.save(deelnemer);
         return "het is gelukt";
+    }
+
+    @GetMapping("/deelnemer/{id}")
+    public Deelnemer getDeelnemerId(@PathVariable Long id){
+        Optional<Deelnemer> deelnemer = mijnSchaakRepository.findById(id);
+        return deelnemer.get();
+    }
+
+    @DeleteMapping("/deelnemer/{id}/delete")
+    public void deleteDeelnemer (@PathVariable Long id){
+        mijnSchaakRepository.deleteById(id);
+        System.out.println("De deelnemer is verwijderd" + id);
+
     }
 
 }
